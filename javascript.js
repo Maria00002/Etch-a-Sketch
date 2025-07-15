9227c335496d173498ccb854dd3e6bb60847f8e8
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-14 18:24:39 -0700

    Create a 16x16 grid of square divs

---
+++ javascript.js
@@ -0,0 +1,27 @@
+const container = document.querySelector("#container");
+let numberOfSquare = 16;
+let numberOfSquareByRow = numberOfSquare;
+let numberOfSquareByColumn = numberOfSquare;
+
+const screenWidth = window.innerWidth;
+console.log(screenWidth);
+
+for (let i = 0; i < numberOfSquareByRow; i++) {
+    // console.log(i);
+    for (let j = 0; j < numberOfSquareByColumn; j++) {
+        const square = document.createElement("div");
+        square.classList.toggle("square");
+        let sizeOfSquare;
+        sizeOfSquare = (screenWidth - 30 * 2)/numberOfSquare;
+        
+        square.style.cssText =`height: ${sizeOfSquare}px; width: ${sizeOfSquare}px; 
+        background-color: #555; border-style: solid; 
+        border-width: thin; box-sizing: border-box`;
+        container.appendChild(square);
+        console.log(sizeOfSquare);
+        // console.log(j);
+    }
+    
+
+}
+
