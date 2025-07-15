bd1ac1a4e5fbeeb5172487c3d91bd210703f8a0c
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-15 15:50:25 -0700

    Hover over can display random color

--- javascript.js
+++ javascript.js
@@ -1,27 +1,60 @@
-const container = document.querySelector("#container");
-let numberOfSquare = 16;
-let numberOfSquareByRow = numberOfSquare;
-let numberOfSquareByColumn = numberOfSquare;
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector("#container");
+    const screenWidth = window.innerWidth;
+    console.log(screenWidth);
 
-const screenWidth = window.innerWidth;
-console.log(screenWidth);
-
-for (let i = 0; i < numberOfSquareByRow; i++) {
-    // console.log(i);
-    for (let j = 0; j < numberOfSquareByColumn; j++) {
-        const square = document.createElement("div");
-        square.classList.toggle("square");
-        let sizeOfSquare;
-        sizeOfSquare = (screenWidth - 30 * 2)/numberOfSquare;
-        
-        square.style.cssText =`height: ${sizeOfSquare}px; width: ${sizeOfSquare}px; 
-        background-color: white; border-style: solid; 
-        border: 1px solid; box-sizing: border-box`;
-        container.appendChild(square);
-        console.log(sizeOfSquare);
-        // console.log(j);
+    function createGrid(numberOfSquareByRow,numberOfSquareByColumn) {
+        for (let i = 0; i < numberOfSquareByRow; i++) {
+            for (let j = 0; j < numberOfSquareByColumn; j++) {
+                const square = document.createElement("div");
+                square.classList.toggle("square");
+                let sizeOfSquare;
+                sizeOfSquare = (screenWidth - 30 * 2)/numberOfSquareByColumn;
+                
+                square.style.cssText =`height: ${sizeOfSquare}px; width: ${sizeOfSquare}px; 
+                background-color: white; border-style: solid; 
+                border: 1px solid; box-sizing: border-box`;
+                container.appendChild(square);
+                
+                square.addEventListener("mouseover", (event) => {
+                    square.style.backgroundColor = `${getRandomColor()}`;
+                });
+            }
     }
+
+
+
+
+
     
 
 }
+    // square.addEventListener("click", (event) => {
+    //     console.log("I have clicked");
+    //     // square.style.cssText = `background-color: ${getRandomColor};`;
+    //     square.style.cssText = `background-color: black;`;
+    // });
+
+
+
+    function getRandomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = "#";
+        for (let i = 0; i < 6 ; i++) {
+            color += letters[Math.floor(Math.random()*16)];
+        }
+        return color;
+    }
+
+    createGrid(16,16);
+
+});
+
+
+
+
+
+
+
+
 
